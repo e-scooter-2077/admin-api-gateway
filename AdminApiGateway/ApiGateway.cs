4a7c8424ba3665ebba2cb23f3d1f320f76563003
Samuele <samuele.bura98@gmail.com>
2021-12-05 22:14:50 +0100

    Add test implementation of scooter function

--- AdminApiGateway/ApiGateway.cs
+++ AdminApiGateway/ApiGateway.cs
@@ -1,5 +1,11 @@
+using System;
 using System.Collections.Generic;
 using System.Net;
+using System.Text.Json;
+using System.Threading.Tasks;
+using Azure;
+using Azure.DigitalTwins.Core;
+using Azure.Identity;
 using Microsoft.Azure.Functions.Worker;
 using Microsoft.Azure.Functions.Worker.Http;
 using Microsoft.Extensions.Logging;
@@ -9,16 +15,36 @@ namespace AdminApiGateway
     public static class ApiGateway
     {
         [Function("scooters")]
-        public static HttpResponseData GetScooters(
+        public static async Task<HttpResponseData> GetScooters(
             [HttpTrigger(AuthorizationLevel.Anonymous, "get")] HttpRequestData req,
             FunctionContext executionContext)
         {
             var logger = executionContext.GetLogger("get-scooters");
+            string digitalTwinUrl = "https://" + Environment.GetEnvironmentVariable("AzureDTHostname");
+            var credential = new DefaultAzureCredential();
+            var digitalTwinsClient = new DigitalTwinsClient(new Uri(digitalTwinUrl), credential);
+
+            var scooters = new List<BasicDigitalTwin>();
+
+            string query = "SELECT * FROM DIGITALTWINS DT WHERE IS_OF_MODEL(DT, 'dtmi:com:escooter:EScooter;1')";
+            AsyncPageable<BasicDigitalTwin> result = digitalTwinsClient.QueryAsync<BasicDigitalTwin>(query);
+            try
+            {
+                await foreach (BasicDigitalTwin twin in result)
+                {
+                    scooters.Add(twin);
+                }
+            }
+            catch (RequestFailedException ex)
+            {
+                Console.WriteLine($"Error {ex.Status}, {ex.ErrorCode}, {ex.Message}");
+                throw;
+            }
 
             var response = req.CreateResponse(HttpStatusCode.OK);
-            response.Headers.Add("Content-Type", "text/plain; charset=utf-8");
+            response.Headers.Add("Content-Type", "application/json; charset=utf-8");
 
-            response.WriteString("Welcome to Azure Functions!");
+            await response.WriteAsJsonAsync(scooters);
 
             return response;
         }
