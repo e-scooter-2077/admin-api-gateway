6bd6ef32e1bd78a6a257d077c98c756b35aaa2c9
ldeluigi <lucadelu97@gmail.com>
2022-01-08 09:22:48 +0100

    Test to see if CI calls unit tests.

---
+++ test/AdminApiGateway.UnitTests/UnitTest1.cs
@@ -0,0 +1,14 @@
+using Shouldly;
+using Xunit;
+
+namespace AdminApiGateway.UnitTests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+            true.ShouldBe(false); // Tests are not implemented
+        }
+    }
+}
